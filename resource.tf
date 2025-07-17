3e38f9c8385720017c289991ea84e0c45728d4c8
hemanthkatta07@gmail.com <hemanthkatta07@gmail.com>
2025-07-17 11:32:53 +0000

    third

--- resource.tf
+++ resource.tf
@@ -2,3 +2,11 @@ resource "azurerm_resource_group" "myterra07" {
   name     = "myterra07"
   location = "East US"
 }
+
+resource "azurerm_storage_account" "mysaterra07" {
+    name                     = "storagehk07"
+    resource_group_name      = azurerm_resource_group.myterra07.name
+    location                 = azurerm_resource_group.myterra07.location
+    account_tier             = "Standard"
+    account_replication_type = "LRS"
+}
